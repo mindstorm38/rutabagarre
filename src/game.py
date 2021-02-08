89cb561951e31563d726d433e4b1b6741f6b516f
Théo Rozier <contact@theorozier.fr>
2021-02-08 11:12:33 +0100

    Structure de base pour la class Game

--- src/game.py
+++ src/game.py
@@ -7,9 +7,26 @@ class Game:
 
     def __init__(self):
         self._screen: Optional[Surface] = None
+        self._running: bool = False
 
     def start(self):
 
+        # Init
         pygame.init()
-
         self._screen = pygame.display.set_mode((1024, 768))
+        self._running = True
+
+        # Loop
+        while self._running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
+
+            self._update()
+
+        # Cleanup
+        pass
+
+    def _update(self):
+        pass
