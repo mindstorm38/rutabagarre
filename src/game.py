4383a91228e02c59bee6b91ce591f484b7f439a9
Théo Rozier <contact@theorozier.fr>
2021-02-08 14:14:27 +0100

    Documentation de Game.

--- src/game.py
+++ src/game.py
@@ -9,6 +9,11 @@ from view.title import TitleView
 
 class Game:
 
+    """
+    Classe principale pour le jeu. A le rôle du controlleur et possède les
+    instances du Stage en cours d'execution ainsi que les vues.
+    """
+
     def __init__(self):
 
         self._surface: Optional[Surface] = None
@@ -20,18 +25,21 @@ class Game:
 
         self._stage: Optional[Stage] = None
 
-        self.init_views()
+        self._init_views()
 
-    def init_views(self):
+    def _init_views(self):
+
+        """
+        Ajout de toutes les vues (à compléter au fur et à mesure).
+        """
+
         self._add_view("title", TitleView())
 
-    def show_view(self, view_name: str):
-        view = self._views.get(view_name)
-        if view is None:
-            raise ValueError("Invalid view name '{}'.".format(view_name))
-        self._view = view
-
     def start(self):
+
+        """
+        Point d'entrée pour le jeu.
+        """
 
         print("=> Starting PyGame...")
 
@@ -69,6 +77,10 @@ class Game:
 
     def _update(self):
 
+        """
+        Appelé à chaque frame afin de mettre à jour l'affichage et la physique.
+        """
+
         if self._stage is not None:
             self._stage.update()
 
@@ -76,6 +88,25 @@ class Game:
             self._view.draw(self._surface)
 
     def _add_view(self, name: str, view: View):
+
+        """
+        Ajoute une vue associée à son nom, utile plus tard pour `show_view`.
+        :param name: Nom de la vue.
+        :param view: Instance de la vue.
+        """
+
         if name is None or view is None:
             raise ValueError("Name and view object can't be None.")
         self._views[name] = view
+
+    def show_view(self, view_name: str):
+
+        """
+        Défini la nouvelle vue à afficher en fonction de son nom lors de
+        son enregistrement.
+        """
+
+        view = self._views.get(view_name)
+        if view is None:
+            raise ValueError("Invalid view name '{}'.".format(view_name))
+        self._view = view
