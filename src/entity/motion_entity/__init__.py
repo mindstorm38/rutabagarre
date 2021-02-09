53c64b39d6ad28dbad1b6e1a9b1614e74f7ace6f
VMoM <ukfg.poubelle@gmail.com>
2021-02-09 12:18:03 +0100

    Starting to work on entity movement

--- src/entity/motion_entity/__init__.py
+++ src/entity/motion_entity/__init__.py
@@ -7,6 +7,8 @@ class MotionEntity(Entity, ABC):
     """
     Abstract subclass of Entity that can move
     """
+    NATURAL_FRICTION = 0.4
+
     def __init__(self, entity_stage: 'stage.Stage') -> None:
         Entity.__init__(self, entity_stage)
         self._vel_x: float = 0.0
@@ -26,10 +28,24 @@ class MotionEntity(Entity, ABC):
     def set_vel_y(self, y: float) -> None:
         self._vel_y = y
 
-    # ADDERS
-    def add_vel_x(self, number: float) -> None:
-        self._vel_x += number
+    # OTHER METHODS
+    def update(self) -> None:
+        self.update_motion()
 
-    def add_vel_y(self, number: float) -> None:
-        self._vel_y += number
+    def update_motion(self) -> None:
+        self.update_natural_velocity()
+        self.move_position()
+
+    def update_natural_velocity(self) -> None:
+        self._vel_x *= MotionEntity.NATURAL_FRICTION
+        self._vel_y *= MotionEntity.NATURAL_FRICTION
+
+    def move_position(self) -> None:
+        # TODO : intégrer la gestion des hitbox
+        self.add_to_x(
+            self.get_vel_x()
+        )
+        self.add_to_y(
+            self.get_vel_y()
+        )
 
