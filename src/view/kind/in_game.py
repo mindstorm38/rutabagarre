f6f25eded648b21ad6ea1a344586774b10de4021
Théo Rozier <contact@theorozier.fr>
2021-02-11 21:18:18 +0100

    Ajout du background de combat.

--- src/view/kind/in_game.py
+++ src/view/kind/in_game.py
@@ -284,6 +284,8 @@ class InGameView(View):
 
         super().__init__()
 
+        self._background_surface: Optional[Surface] = None
+
         self._terrain_tilemap: Optional[TileMap] = None
         self._item_tilemap: Optional[TileMap] = None
         self._player_anim_surface: Optional[AnimSurfaceColored] = None
@@ -311,6 +313,8 @@ class InGameView(View):
     def on_enter(self):
 
         print("Loading stage...")
+
+        self._background_surface = self._shared_data.get_image("fightbackground.png")
 
         pygame.mixer.music.load("../res/music/fightmusic.ogg")
         pygame.mixer.music.play(1)
@@ -372,8 +376,10 @@ class InGameView(View):
 
         self._y_offset = unscaled_size[1] - (((height - old_height) // 2) + 1) * self.TILE_SIZE
 
-        self._terrain_surface = Surface(unscaled_size, 0, self._shared_data.get_game().get_surface())
-        self._final_surface = Surface(unscaled_size, 0, self._shared_data.get_game().get_surface())
+        self._terrain_surface = Surface(unscaled_size, pygame.HWSURFACE, self._background_surface)
+        self._final_surface = Surface(unscaled_size, pygame.HWSURFACE)
+
+        pygame.transform.scale(self._background_surface, unscaled_size, self._terrain_surface)
 
         for x, y, tile_id in self._stage.for_each_tile():
             tile_name = self.TILES_NAMES.get(tile_id)
@@ -404,7 +410,7 @@ class InGameView(View):
             )
 
             if self._scaled_surface is None:
-                self._scaled_surface = Surface(scaled_size, 0, self._shared_data.get_game().get_surface())
+                self._scaled_surface = Surface(scaled_size, pygame.HWSURFACE)
 
             self._scaled_surface_pos = (-scaled_size[0] * start_ratio, (surface_height - scaled_size[1]) / 2)
 
@@ -446,12 +452,10 @@ class InGameView(View):
 
     def _inner_pre_draw(self, surface: Surface):
 
-        surface.fill((0, 0, 0))
-
         if self._stage is None:
+            surface.fill((0, 0, 0))
             return
 
-        self._final_surface.fill((0, 0, 0))
         self._final_surface.blit(self._terrain_surface, (0, 0))
 
         # Camera
